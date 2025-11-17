f71d49948b5987bee8b1b1c3c79c26a762e53c5c
Dera2S <dera.ah.14@gmail.com>
2025-11-17 12:11:12 +0300

    update : add utils and update card UI

--- resources/js/Pages/LandingPage.jsx
+++ resources/js/Pages/LandingPage.jsx
@@ -1,11 +1,16 @@
 import { LandingPage } from '@/pages/landingPage';
 import { ProductProvider } from '@/pages/landingPage/context/productProvider';
-
+import { Modalprovider } from '@/pages/landingPage/context/useModalProvider';
+import { ThemeProvider } from '@/theme/useThemeProvider';
 const landing = () => {
     return (
-        <ProductProvider>
-            <LandingPage />
-        </ProductProvider>
+        <ThemeProvider>
+            <ProductProvider>
+                <Modalprovider>
+                    <LandingPage />
+                </Modalprovider>
+            </ProductProvider>
+        </ThemeProvider>
     );
 };
 
